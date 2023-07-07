ff31cc214564dbbb900675b8fdee308a853b0c81
Davi Menezes <davimenezes.dev@gmail.com>
2023-07-06 21:55:33 -0300

    improvements

---
+++ Repositories/PaymentRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Modules\MercadoPago\Repositories;
+
+use Modules\Base\Repository\BaseRepository;
+use Modules\MercadoPago\Models\PaymentModel;
+
+/**
+ * @author Davi Menezes(davimenezes.dev@gmail.com)
+ * @link https://github.com/DaviMenezes
+ * @method self obj()
+ * @method PaymentModel model()
+ * @method PaymentModel find($id)
+ * @method PaymentModel first()
+ * @method PaymentModel findOrNew($id)
+ * @method PaymentModel firstOrNew($query)
+ * @method PaymentModel findOrFail($id)
+ */
+class PaymentRepository extends BaseRepository
+{
+    /**
+     * @inheritDoc
+     */
+    public function modelClass(): string
+    {
+        return PaymentModel::class;
+    }
+}
---
+++ Repositories/PreferenceRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Modules\MercadoPago\Repositories;
+
+use Modules\Base\Repository\BaseRepository;
+use Modules\MercadoPago\Entities\Preference\PreferenceEntityModel;
+use Modules\MercadoPago\Models\PreferenceModel;
+
+/**
+ * @author Davi Menezes(davimenezes.dev@gmail.com)
+ * @link https://github.com/DaviMenezes
+ * @method self obj()
+ * @method PreferenceModel model()
+ * @method PreferenceEntityModel find($id)
+ * @method PreferenceModel first()
+ * @method PreferenceModel findOrNew($id)
+ * @method PreferenceModel firstOrNew($query)
+ * @method PreferenceEntityModel findOrFail($id)
+ */
+class PreferenceRepository extends BaseRepository
+{
+    /**
+     * @inheritDoc
+     */
+    public function modelClass(): string
+    {
+        return PreferenceModel::class;
+    }
+}
