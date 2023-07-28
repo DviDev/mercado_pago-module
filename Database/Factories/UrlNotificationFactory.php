62a7fba3709fd753c17741978deb5dfbe648f962
Davi Menezes <davimenezes.dev@gmail.com>
2023-07-28 18:58:19 -0300

    🪴seeder improvements

--- Database/Factories/UrlNotificationFactory.php
+++ Database/Factories/UrlNotificationFactory.php
@@ -3,6 +3,7 @@
 namespace Modules\MercadoPago\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Modules\Base\Factories\BaseFactory;
 use Modules\MercadoPago\Models\UrlNotificationModel;
 use Modules\MercadoPago\Entities\UrlNotification\UrlNotificationEntityModel;
 
@@ -10,7 +11,7 @@ use Modules\MercadoPago\Entities\UrlNotification\UrlNotificationEntityModel;
  * @method UrlNotificationModel create(array $attributes = [])
  * @method UrlNotificationModel make(array $attributes = [])
  */
-class UrlNotificationFactory extends Factory
+class UrlNotificationFactory extends BaseFactory
 {
     /**
      * The name of the factory's corresponding model.
