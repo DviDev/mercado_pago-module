f73137e9363ade6c9ca875c4d9d7d6d55df7e572
Davi Menezes <davimenezes.dev@gmail.com>
2023-11-08 23:57:32 -0300

    clean code

---
+++ Database/Migrations/2023_11_07_235951_update_mp_payments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Modules\MercadoPago\Entities\Payment\PaymentEntityModel;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('mp_payments', function (Blueprint $table) {
+            $p = PaymentEntityModel::props(force: true);
+            $table->string($p->external_reference)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('', function (Blueprint $table) {
+
+        });
+    }
+};
