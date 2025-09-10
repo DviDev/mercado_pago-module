2409ebefa26428ccec1db5e700b9143069bf503e
Davi Menezes <davimenezes.dev@gmail.com>
2025-09-10 14:09:39 -0300

    Remove unused queue-related imports from MercadoPago listeners

--- app/Listeners/CreateMenuItemsMercadoPagoListener.php
+++ app/Listeners/CreateMenuItemsMercadoPagoListener.php
@@ -2,8 +2,6 @@
 
 namespace Modules\MercadoPago\Listeners;
 
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Modules\Project\Contracts\CreateMenuItemsListenerContract;
 
 class CreateMenuItemsMercadoPagoListener extends CreateMenuItemsListenerContract
--- app/Listeners/DefineSearchableMercadoPagoAttributes.php
+++ app/Listeners/DefineSearchableMercadoPagoAttributes.php
@@ -2,8 +2,6 @@
 
 namespace Modules\MercadoPago\Listeners;
 
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Modules\Project\Contracts\DefineSearchableAttributesContract;
 
 class DefineSearchableMercadoPagoAttributes extends DefineSearchableAttributesContract
